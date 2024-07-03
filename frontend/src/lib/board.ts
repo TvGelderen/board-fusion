04a6843100ea554462b5e1c0b741639616624826
Timen van Gelderen <thvangelderen@gmail.com>
2024-07-03 20:14:22 +0200

    expanded drawing

--- frontend/src/lib/board.ts
+++ frontend/src/lib/board.ts
@@ -8,10 +8,31 @@ export function initializeBoard() {
     canvas.width = canvas.offsetWidth;
     canvas.height = canvas.offsetHeight;
     
+    let drawing = false;
+    
+    const startDrawing = () => {
+        drawing = true;
+        ctx.beginPath();
+    };
+    
+    const stopDrawing = () => {
+        drawing = false;
+    };
+
+    const handleMouseEnter = () => {
+        if (!drawing) return;
+        ctx.beginPath();
+    };
+    
     const drawLine = (e: MouseEvent) => {
+        if (!drawing) return;
         ctx.lineTo(e.offsetX, e.offsetY);
         ctx.stroke();
-    }
+    };
     
+    canvas.addEventListener("mousedown", startDrawing);
+    canvas.addEventListener("mouseenter", handleMouseEnter);
     canvas.addEventListener("mousemove", drawLine);
+    canvas.addEventListener("mouseleave", drawLine);
+    document.addEventListener("mouseup", stopDrawing);
 }
