2dcd34cd1327636203bed273816d2f95ed36af72
Timen van Gelderen <thvangelderen@gmail.com>
2024-07-10 07:29:52 +0200

    changed layout

--- frontend/src/routes/Canvas.svelte
+++ frontend/src/routes/Canvas.svelte
@@ -2,26 +2,17 @@
     import { onMount } from 'svelte';
     import { EAction, ETool, initializeBoard, updateAction, updateTool } from '$lib/board';
 	import IconButton from './IconButton.svelte';
-    import Icon from '@iconify/svelte';
 
     onMount(() => {
         initializeBoard();
     });
 </script>
 
-<div class="flex justify-between items-center p-2 shadow-md bg-white">
-    <div class="flex">
-        <Icon icon="material-symbols:brush-outline" height="36" />
-        <h2 class="text-3xl ml-2">Board Fusion</h2>
-    </div>
-    <div class="flex gap-2">
-        <IconButton icon="mdi:cursor-default-outline" onclick={() => updateAction(EAction.Drag)} />
-        <IconButton icon="mdi:pencil-outline" onclick={() => updateTool(ETool.Pencil)} />
-        <IconButton icon="mdi:crop-square" onclick={() => updateTool(ETool.Rectangle)} />
-        <IconButton icon="mdi:vector-line" onclick={() => updateTool(ETool.Line)} />
-    </div>
+<div class="absolute flex gap-2 bg-white rounded-md shadow-xl p-2 top-2 left-[50%] translate-x-[-50%]">
+    <IconButton icon="mdi:cursor-default-outline" onclick={() => updateAction(EAction.Drag)} />
+    <IconButton icon="mdi:pencil-outline" onclick={() => updateTool(ETool.Pencil)} />
+    <IconButton icon="mdi:crop-square" onclick={() => updateTool(ETool.Rectangle)} />
+    <IconButton icon="mdi:vector-line" onclick={() => updateTool(ETool.Line)} />
 </div>
 
-<div class="w-full h-full p-4">
-    <canvas class="w-full h-full shadow-xl rounded-md bg-white"></canvas>
-</div>
+<canvas class="w-full h-full bg-white"></canvas>
